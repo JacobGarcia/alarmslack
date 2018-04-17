d263b0910a56535c7687e8320910474a924a2ae7
Frans <franscaraveli@gmail.com>
2018-04-17 15:51:41 -0500

    Ya chucha

---
+++ slack.py
@@ -0,0 +1,42 @@
+import requests
+import websocket
+import json
+import time
+from playsound import playsound
+def on_message(ws, message):
+    message = json.loads(message)
+    print (message)
+    print (message["text"])
+    if message["text"] == "*Build started*":
+        playsound("audio.mp3")
+        print ("sonaaar")
+
+def on_error(ws, error):
+    print (error)
+
+def on_close(ws):
+    print ("### closed ###")
+
+def on_open(ws):
+    time.sleep(1)
+    print ("thread terminating...")
+
+
+
+if __name__ == "__main__":
+    token = "xoxp-63699683760-325096873269-347797851971-cd9296027ecc70d3c90b9cada1cfd9fe"
+    headers = {
+        'content-type': "application/x-www-form-urlencoded",
+        'Authorization': 'Bearer ' + token,
+        'cache-control': "no-cache",
+    }
+    r = requests.get('https://slack.com/api/rtm.connect',headers=headers)
+    websock = (r.json()["url"])
+    ws = websocket.WebSocketApp(websock,
+                                on_message = on_message,
+                                on_error = on_error,
+                                on_close = on_close)
+    ws.on_open = on_open
+    ws.run_forever()
+
+    
